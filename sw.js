8de5202fd59be395ef59c33a8d47ed825b0f2721
thunder11260121 <blackthunder1115@hotmail.co.jp>
2025-08-09 10:59:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wbgt-split-v2';
+const CACHE='wbgt-split-v3';
 const ASSETS=['./','index.html','spots.html','styles.css','weather.js','spots.js','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
